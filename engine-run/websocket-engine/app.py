844e99aa0cd579b36285c77b037c39d9a0fb0720
Joanna Hsu <joanna@Joannas-MacBook-Pro.local>
2016-12-27 20:16:44 -0800

    update engine-run

---
+++ engine-run/websocket-engine/app.py
@@ -0,0 +1,159 @@
+from flask import Flask, render_template,request
+from flask_socketio import SocketIO,send,emit
+import boto.sns,json,inspect
+from flask_socketio import join_room, leave_room
+sns_conn = boto.sns.connect_to_region("us-east-1", profile_name='movie')
+print sns_conn.aws_access_key_id
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+#online = {}
+SidToUid = {}
+
+
+# def cleanUserByUid(uid):
+#     try :
+#         sid = online[uid]
+#         if uid in online: del online[uid]
+#         if sid in SidToUid: del SidToUid[sid]
+#     except Exception as e:
+#           print e 
+# 
+# def cleanUserBySid(sid):
+#     try : 
+#         uid = SidToUid[sid]
+#         if uid in online: del online[uid]
+#         if sid in SidToUid: del SidToUid[sid]
+#     except Exception as e:
+#           print e    
+
+@socketio.on('connect')
+def remote():
+    print request.sid
+    # print [name for name,thing in inspect.getmembers(request)]
+    print 'remote connected ip: %s , %s'%(request.remote_addr,request.user_agent)
+
+@socketio.on('disconnect')
+def close():
+	#cleanUserBySid(request.sid)
+	if request.sid in SidToUid:
+		cur_room = SidToUid[request.sid] #####################
+		leave_room(cur_room)
+    #print 'remote close ip: %s'%request.remote_addr
+    # print online
+    
+        # print online
+        # for k,v in online.items():
+            # send to specific user
+            # emit('message',uid+" left",room=v)
+        # print request.sid
+ 
+
+@socketio.on('init')
+def handle_my_custom_event(json_data):
+    print('received json_data: ' + str(json_data))
+
+
+@socketio.on('watch_interval')
+def handle_my_custom_event(json_data):
+    # print json_data["uid"]
+    # print 'remote connected ip: %s'%request.remote_addr
+    uid = json_data["uid"]
+    if uid != "non-login":
+		join_room(uid)
+		#online[uid] = request.sid
+		SidToUid[request.sid] = uid
+    json_data["remote_addr"] = request.remote_addr
+    json_data["event"] = 'watch_interval'
+    print json.dumps(json_data)
+    sns_conn.publish(
+					topic="arn:aws:sns:us-east-1:612129620405:watch_interval",
+					message=json.dumps(json_data)
+	)
+    # print('received watch: ' + str(json_data))
+
+@socketio.on('click_video')
+def click_video(json_data):
+    # print json_data["uid"]
+    # print 'remote connected ip: %s'%request.remote_addr
+    uid = json_data["uid"]
+    if uid != "non-login":
+        #online[uid] = request.sid
+        SidToUid[request.sid] =uid
+        join_room(uid)
+
+    json_data["remote_addr"] = request.remote_addr
+    json_data["event"] = 'click_video'
+    print json.dumps(json_data)
+    sns_conn.publish(
+					topic="arn:aws:sns:us-east-1:612129620405:watch_interval",
+					message=json.dumps(json_data)
+	)
+
+@socketio.on('rec_list')
+def rec_list(json_data):
+    # print json_data["uid"]
+    # print 'remote connected ip: %s'%request.remote_addr
+    uid = json_data["uid"]
+    if uid != "non-login":
+        #online[uid] = request.sid
+        SidToUid[request.sid] =uid
+        join_room(uid)
+
+    json_data["remote_addr"] = request.remote_addr
+    json_data["event"] = 'rec_list'
+    print json.dumps(json_data)
+    sns_conn.publish(
+					topic="arn:aws:sns:us-east-1:612129620405:watch_interval",
+					message=json.dumps(json_data)
+	)
+
+@socketio.on('recommendUser')
+def handle_message(message):
+    # print('received message: ' + message)
+    data = json.loads(message)
+    print data
+    sendRecommendation(data["uid"], data["rec_list"])
+
+
+def sendRecommendation(uid, rec_list):
+    if uid in SidToUid.values():
+        data = {}
+        data["action"] = "rec"
+        data["rec_list"] = rec_list
+        try:
+            emit('message',json.dumps(data),room=uid)
+        except Exception as e:
+            cleanUserByUid(uid)
+            print e 
+
+@socketio.on('recommendUserScene')
+def handleScene_message(message):
+    # print('received message: ' + message)
+    data = json.loads(message)
+    print data
+    sendSceneRecommendation(data["uid"], data["rec_list"])
+
+
+def sendSceneRecommendation(uid, rec_list):
+    if uid in SidToUid.values():
+        data = {}
+        data["action"] = "recScene"
+        data["rec_list"] = rec_list
+        try:
+            emit('message',json.dumps(data),room=uid)
+        except Exception as e:
+            cleanUserByUid(uid)
+            print e
+
+
+if __name__ == '__main__':
+
+    #open external connections
+    host = '0.0.0.0'
+    port = 6888
+    print "WebSocket engine listen @ %d"%port
+    socketio.run(app, host=host,port=port)
+
---
+++ engine-run/websocket-engine/peer.py
@@ -0,0 +1,31 @@
+
+from socketIO_client import SocketIO
+import time,json
+
+def on_connect():
+    print('connect')
+
+def on_disconnect():
+    print('disconnect')
+
+def on_reconnect():
+    print('reconnect')
+
+def on_aaa_response(*args):
+    print('on_aaa_response', args)
+
+socketIO = SocketIO('http://54.221.40.5:8111', 6888)
+socketIO.on('connect', on_connect)
+socketIO.on('disconnect', on_disconnect)
+socketIO.on('reconnect', on_reconnect)
+
+while 1:
+
+	data ={}
+	data["uid"]="cl3469@gmail.com"
+	data["rec_list"] = [("Quadrille (1938)","Comedy|Romance"),("Tall Man, The (2012)","Crime|Drama|Mystery")]
+	print json.dumps(data)
+	socketIO.emit('recommendUser',json.dumps(data))
+	socketIO.emit('recommendUserScene',json.dumps(data))
+	time.sleep(10)
+
