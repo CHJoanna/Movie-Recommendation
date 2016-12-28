0bb9e2fb215e4a696e3b87c198483075dd48b020
Joanna Hsu <joanna@Joannas-MacBook-Pro.local>
2016-12-27 20:14:10 -0800

    add engine-run folder

---
+++ engine-run/worker/worker.py
@@ -0,0 +1,8 @@
+import boto.sqs,boto.sns,json, inspect, threading, logging, time, requests
+conn = boto.sqs.connect_to_region("us-east-1",profile_name='movie')
+q=conn.get_queue('watch_interval')
+while True:
+	for message in q.get_messages():
+		body = json.loads(message.get_body())
+		msg =json.loads(body["Message"])
+		print body["Message"]
